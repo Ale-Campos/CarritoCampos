cf08b521a399e6f2bfe127566d46e59ca41f8703
Ale-Campos <alejandrocampos9092@gmail.com>
2023-11-20 20:04:10 -0300

    Agrego objetos y operadores en arrays

--- main.js
+++ main.js
@@ -1,31 +1,56 @@
+class User {
+    constructor(username, password) {
+        this.username = username;
+        this.password = password;
+    }
+}
+
+class Product {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+}
+
+
 const authUsers = ["user", "test", "admin", "developer"];
+const authPasswords = ["user1234", "test90921", "admin22884", "developer0011"];
+const usersList = generateAuthUserList(authUsers, authPasswords);
 const productList = [
   "celular",
   "televisor",
   "tablet",
   "laptop",
   "heladera",
-  "salir",
 ];
 const prices = [300000, 250000, 400000, 600000, 400000];
+const productsList = generateProductList(productList, prices);
 
-let loggedUser;
+
+let loggedUsername;
+let loggedPassword;
 let authorized = false;
 let maxAttempts = 3;
 let currentAttempts = 0;
 let finalPrice = 0;
 
 do {
-  loggedUser = prompt("Ingrese su nombre de usuario");
-  serchUser(loggedUser);
+  loggedUsername = prompt("Ingrese su nombre de usuario");
+  loggedPassword = prompt("Ingrese su contraseña");
+  let loggedUser = usersList.find(user => user.username == loggedUsername);
+  if(loggedUser != undefined & loggedUser.password === loggedPassword) {
+        authorized = true;
+  } else {
+        alert("Usuario o contraseña incorrectos");
+  }
   currentAttempts++;
 } while (!authorized && currentAttempts < maxAttempts);
 
 if (authorized) {
     offerProducts();
-    alert("Compra realizada! El precio final es: $" + finalPrice)
+    alert("Compra realizada! El precio final es: $" + finalPrice);
 } else {
-  console.log("Usuario no autorizado");
+  alert("No se pudo iniciar sesión, intente nuevamente más tarde");
 }
 
 function offerProducts() {
@@ -33,18 +58,15 @@ function offerProducts() {
     
 
   do {
-    let messsage =
-    `Ingresa algunos de los productos o "Salir" para terminar tu compra \n Celular: $300.000 \n Televisor: $250.000 
-    \n Tablet: $400.000 \n Laptop: $600.000 \n Heladera: $400.000 
-    \n Precio final: ${finalPrice}`;
+    let messsage = generateOfferMessage(productsList, finalPrice)
     let productName = prompt(messsage);
 
     if(productName.toLocaleLowerCase() != "salir") {
-        let price = searchProduct(productName);
-        if(price == 0) {
-            alert("El producto ingresado no se encuentra en la lista")
+        let selectedProduct = productsList.find(product => product.name == productName.toLowerCase());
+        if(selectedProduct != undefined) {
+            finalPrice += selectedProduct.price;
         } else {
-            finalPrice += price;
+            alert("El producto ingresado no se encuentra en la lista")
         }
     } else {
         endBuy = true;
@@ -53,28 +75,35 @@ function offerProducts() {
   } while (!endBuy);
 }
 
-function serchUser(userName) {
-    let index = 0;
-    while(!authorized && index<authUsers.length) {
-        if(authUsers[index] == userName) {
-            authorized = true;
-        } else {
-            index++;
+function generateAuthUserList(usernameList, passwordList) {
+    let users = [];
+    if(usernameList.length === passwordList.length) {
+        for(let i=0; i<usernameList.length; i++) {
+            let user = new User(usernameList[i], passwordList[i]);
+            users.push(user);
         }
+    } else {
+        console.log("La cantidad de usuarios y contraseñas no coincide");
     }
+    return users;
 }
 
-function searchProduct(productName) {
-    let prodFound = false;
-    let index = 0;
-    let price = 0;
-    while(!prodFound && index<productList.length) {
-        if(productList[index] == productName.toLowerCase()) {
-            prodFound = true;
-            price = prices[index];
-        } else {
-            index++;
-        }
+function generateProductList(productList, prices) {
+    let products = [];
+    for(let i=0; i<productList.length; i++) {
+        let product = new Product(productList[i], prices[i]);
+        products.push(product);
     }
-    return price;
+    return products;
 }
+
+function generateOfferMessage(productList, finalPrice) {
+    let initialMessage = 'Ingresa algunos de los productos o "Salir" para terminar tu compra';
+
+    productList.forEach(product => {
+        initialMessage += `\n ${product.name}: ${product.price}`;
+    });
+    initialMessage += `\n salir \n Precio final: ${finalPrice}`;
+   return initialMessage;
+}
+
