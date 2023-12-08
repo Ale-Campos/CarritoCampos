6480d50c7f1ee006b52586cadc8300b833a42008
Ale-Campos <alejandrocampos9092@gmail.com>
2023-12-08 17:04:16 -0300

    Agrego buscador de productos

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ class User {
 }
 
 class Product {
-  constructor(name, price) {
+  constructor(name, price, id) {
+    this.id = id;
     this.name = name;
     this.price = price;
   }
@@ -19,13 +20,13 @@ const productList = ["celular", "televisor", "tablet", "laptop", "heladera"];
 const prices = [300000, 250000, 400000, 600000, 400000];
 const productsList = generateProductList(productList, prices);
 
-
 let loggedUsername;
 let loggedPassword;
 let maxAttempts = 3;
 let currentAttempts = 0;
 let finalPrice = 0;
 let cart = [];
+let search = "";
 
 function login(event) {
   event.preventDefault();
@@ -36,7 +37,6 @@ function login(event) {
   let loggedUser = usersList.find((user) => user.username == loggedUsername);
 
   if (loggedUser != undefined && loggedUser.password === loggedPassword) {
-
     start();
 
     //TODO:
@@ -66,57 +66,71 @@ function notAllowed() {
   alert("No se pudo iniciar sesión, intente nuevamente más tarde");
 }
 
-function showPoducts() {
-  let prodContainer = document.getElementById("products");
-    if(localStorage.getItem("cart") !== null){
-        cart = JSON.parse(localStorage.getItem("cart"));
-        updateCartView(cart);
-    }
-  productsList.map((product, index) => {
+function searchProduct() {
+  search = document.getElementById("search").value;
+  showPoducts(search);
+}
+
+function showPoducts(search) {
+  let prodContainer = document.getElementById("products-list");
+  prodContainer.innerHTML = "";
+
+  if (search) {
+    filterProducts = productsList.filter((product) =>
+      product.name.includes(search.toLowerCase())
+    );
+  } else {
+    filterProducts = productsList;
+  }
+  if (localStorage.getItem("cart") !== null) {
+    cart = JSON.parse(localStorage.getItem("cart"));
+    updateCartView(cart);
+  }
+  filterProducts.map((product, index) => {
     let prod = document.createElement("article");
     prod.innerHTML = `
             <h3 style="text-transform: capitalize">${product.name}</h3>
             <p>${product.price}</p>
-            <button type="button" onclick="addToCart(${index})">Agregar a carrito</button>
+            <button type="button" onclick="addToCart(${product.id})">Agregar a carrito</button>
         `;
     prodContainer.appendChild(prod);
   });
+  document.getElementById("products").style.display = "block";
   prodContainer.style.display = "block";
   document.getElementById("cart").style.display = "block";
 }
 
-function addToCart(index) {
-    if(localStorage.getItem("cart") === null){
+function addToCart(id) {
+  if (localStorage.getItem("cart") === null) {
+    localStorage.setItem("cart", JSON.stringify([]));
+  }
+  cart = JSON.parse(localStorage.getItem("cart"));
 
-        localStorage.setItem("cart", JSON.stringify([]));
-    }
-    cart = JSON.parse(localStorage.getItem("cart"));
-
-    cart.push(productsList[index]);
-    localStorage.setItem("cart", JSON.stringify(cart));
-    updateCartView(cart);
+  cart.push(productsList.find((product) => product.id === id));
+  localStorage.setItem("cart", JSON.stringify(cart));
+  updateCartView(cart);
 }
 
 function updateCartView(cart) {
-    finalPrice = 0;
-    cartListView = document.getElementById("cart-list");
-    cartListView.innerHTML = "";
-    cart.forEach((product) => {
-        let listItem = document.createElement("li");
-        listItem.innerHTML = `
+  finalPrice = 0;
+  cartListView = document.getElementById("cart-list");
+  cartListView.innerHTML = "";
+  cart.forEach((product) => {
+    let listItem = document.createElement("li");
+    listItem.innerHTML = `
             <h4 style="text-transform: capitalize">${product.name}</h4>
             <p>${product.price}</p>
         `;
-        finalPrice += product.price;
-        cartListView.appendChild(listItem);
-    })
-    document.getElementById("final-price").innerHTML = finalPrice;
+    finalPrice += product.price;
+    cartListView.appendChild(listItem);
+  });
+  document.getElementById("final-price").innerHTML = finalPrice;
 }
 
 function checkout() {
-    alert("Gracias por su compra");
-    localStorage.removeItem("cart");
-    updateCartView([]);
+  alert("Gracias por su compra");
+  localStorage.removeItem("cart");
+  updateCartView([]);
 }
 
 function generateAuthUserList(usernameList, passwordList) {
@@ -135,7 +149,7 @@ function generateAuthUserList(usernameList, passwordList) {
 function generateProductList(productList, prices) {
   let products = [];
   for (let i = 0; i < productList.length; i++) {
-    let product = new Product(productList[i], prices[i]);
+    let product = new Product(productList[i], prices[i], i);
     products.push(product);
   }
   return products;
